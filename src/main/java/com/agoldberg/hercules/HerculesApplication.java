e4e1c0a46ad70c6c7945b99f469190858948614a
Alexander Goldberg <alexander.goldberg25@uga.edu>
2018-05-08 17:39:13 -0400

    Initial commit

---
+++ src/main/java/com/agoldberg/hercules/HerculesApplication.java
@@ -0,0 +1,12 @@
+package com.agoldberg.hercules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerculesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerculesApplication.class, args);
+	}
+}
