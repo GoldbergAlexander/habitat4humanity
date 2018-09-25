92cdbd16a8d55ab7e93fb35d4036a058fdf5a0cd
Alexander Goldberg <alexander.goldberg25@uga.edu>
2018-09-25 18:01:50 -0400

    added file upload support

--- src/main/java/com/agoldberg/hercules/HerculesApplication.java
+++ src/main/java/com/agoldberg/hercules/HerculesApplication.java
@@ -1,8 +1,11 @@
 package com.agoldberg.hercules;
 
+import com.agoldberg.hercules.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,10 +14,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories
 @EnableAsync
-@EnableAutoConfiguration
 public class HerculesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerculesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
--- src/main/java/com/agoldberg/hercules/controller/UserController.java
+++ src/main/java/com/agoldberg/hercules/controller/UserController.java
@@ -5,6 +5,7 @@ import com.agoldberg.hercules.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
+@Profile("!emailless")
 public class UserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
